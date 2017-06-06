a2cbfbdd2d5b8dadb4f68eb81296132fd1ae3d3d
matsutomo <matsumura@goodpatch.com>
2017-06-06 17:57:03 +0900

    Hook view configuration

---
+++ library/src/main/java/com/squareup/timessquare/MonthViewConfigurator.java
@@ -0,0 +1,9 @@
+package com.squareup.timessquare;
+
+/**
+ * Created by matsumura on 2017/06/06.
+ */
+
+public interface MonthViewConfigurator {
+    void configure(MonthView monthView, MonthDescriptor monthDescriptor);
+}
